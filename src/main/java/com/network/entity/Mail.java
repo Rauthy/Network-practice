25f6ccc2cb25082613f4afaa7f581bb70c726d09
liyihan <liyihan@outlook.com>
2019-04-17 10:30:30 +0800

    update entities

---
+++ src/main/java/com/network/entity/Mail.java
@@ -0,0 +1,110 @@
+package com.network.entity;
+
+import java.util.Date;
+
+public class Mail {
+
+    private int mid;
+    private int uid;
+    private String to;//收件人地址
+    private String from;//发件人地址
+    private String subject;
+    private String content;
+    private Date stime;
+    private boolean isDel;//1回收站 0原位
+    private boolean isReceive;//1收件 0发件
+    private boolean isRead;//1收件箱已读 0收件箱未读
+    private int sendCond;//0发送失败，草稿箱 1发送成功，已发送 2正在发送，发件箱
+
+    public int getMid() {
+        return mid;
+    }
+
+    public void setMid(int mid) {
+        this.mid = mid;
+    }
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getStime() {
+        return stime;
+    }
+
+    public void setStime(Date stime) {
+        this.stime = stime;
+    }
+
+    public boolean isDel() {
+        return isDel;
+    }
+
+    public void setDel(boolean del) {
+        isDel = del;
+    }
+
+    public boolean isReceive() {
+        return isReceive;
+    }
+
+    public void setReceive(boolean receive) {
+        isReceive = receive;
+    }
+
+    public boolean isRead() {
+        return isRead;
+    }
+
+    public void setRead(boolean read) {
+        isRead = read;
+    }
+
+    public int getSendCond() {
+        return sendCond;
+    }
+
+    public void setSendCond(int sendCond) {
+        this.sendCond = sendCond;
+    }
+
+
+
+
+}
---
+++ src/main/java/com/network/entity/User.java
@@ -0,0 +1,130 @@
+package com.network.entity;
+
+import java.util.List;
+
+public class User {
+
+    private int uid;
+    private String username;
+    private String email;
+    private String password;
+    private String smtp;
+    private String pop3;
+//    private int role; //0普通用户，1管理员
+
+    private List<Mail> inbox;//收件箱
+    private List<Mail> inbox_unread;//收件箱未读邮件
+    private List<Mail> sent;//已发送
+    private List<Mail> drafts;//草稿箱
+    private List<Mail> outbox;//发件箱
+    private List<Mail> trash;//回收站
+
+
+    public User(){}
+
+    public int getUid() {
+        return uid;
+    }
+
+    public void setUid(int uid) {
+        this.uid = uid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSmtp() {
+        return smtp;
+    }
+
+    public void setSmtp(String smtp) {
+        this.smtp = smtp;
+    }
+
+    public String getPop3() {
+        return pop3;
+    }
+
+    public void setPop3(String pop3) {
+        this.pop3 = pop3;
+    }
+
+//    public int getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(int role) {
+//        this.role = role;
+//    }
+
+    public List<Mail> getInbox() {
+        return inbox;
+    }
+
+    public void setInbox(List<Mail> inbox) {
+        this.inbox = inbox;
+    }
+
+    public List<Mail> getInbox_unread() {
+        return inbox_unread;
+    }
+
+    public void setInbox_unread(List<Mail> inbox_unread) {
+        this.inbox_unread = inbox_unread;
+    }
+
+    public List<Mail> getSent() {
+        return sent;
+    }
+
+    public void setSent(List<Mail> sent) {
+        this.sent = sent;
+    }
+
+    public List<Mail> getDrafts() {
+        return drafts;
+    }
+
+    public void setDrafts(List<Mail> drafts) {
+        this.drafts = drafts;
+    }
+
+    public List<Mail> getOutbox() {
+        return outbox;
+    }
+
+    public void setOutbox(List<Mail> outbox) {
+        this.outbox = outbox;
+    }
+
+    public List<Mail> getTrash() {
+        return trash;
+    }
+
+    public void setTrash(List<Mail> trash) {
+        this.trash = trash;
+    }
+
+
+}
